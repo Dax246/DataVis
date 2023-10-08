d15b2424f85e609126e43467e7cc3d3b17d0e48b
Damien <67739322+Dax246@users.noreply.github.com>
2023-10-08 17:53:55 +1100

    All in one folder and new data file

--- vega_lite.js
+++ vega_lite.js
@@ -1,6 +1,6 @@
 
 
-var vg_2 = "practice.vg.json";
+var vg_2 = "barchart.vg.json";
 vegaEmbed("#bar_chart", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
