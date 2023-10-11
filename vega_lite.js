8a8598fe319987df739456e04518d72452cc8012
Damien <67739322+Dax246@users.noreply.github.com>
2023-10-11 12:37:32 +1100

    Week 10 HW

--- vega_lite.js
+++ vega_lite.js
@@ -7,18 +7,3 @@ var vg_2 = "barchart.vg.json";
 vegaEmbed("#bar_chart", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-
-// var vg_3 = "horizontal_barchart.vg.json";
-// vegaEmbed("#horizontal_bar_chart", vg_3).then(function(result) {
-//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-// }).catch(console.error);
-
-var vg_4 = "scatterplot.vg.json";
-vegaEmbed("#scatterplot", vg_4).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-
-var vg_5 = "stackedbarchart.vg.json";
-vegaEmbed("#stackedbar_chart", vg_5).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
