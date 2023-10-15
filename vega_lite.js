5a59f53231a6c80ba2078c818fd6a4ff7a2c322c
Damien <67739322+Dax246@users.noreply.github.com>
2023-10-15 23:37:00 +1100

    Finished Assignment Code

--- vega_lite.js
+++ vega_lite.js
@@ -1,24 +1,29 @@
 var vg_1 = "map.vg.json";
-vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+vegaEmbed("#choropleth_map", vg_1, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "barchart.vg.json";
-vegaEmbed("#bar_chart", vg_2).then(function(result) {
+vegaEmbed("#bar_chart", vg_2, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-// var vg_3 = "horizontal_barchart.vg.json";
-// vegaEmbed("#horizontal_bar_chart", vg_3).then(function(result) {
-//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-// }).catch(console.error);
+var vg_3 = "horizontal_barchart.vg.json";
+vegaEmbed("#horizontal_bar_chart", vg_3, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
 
 var vg_4 = "scatterplot.vg.json";
-vegaEmbed("#scatterplot", vg_4).then(function(result) {
+vegaEmbed("#scatterplot", vg_4, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_5 = "stackedbarchart.vg.json";
-vegaEmbed("#stackedbar_chart", vg_5).then(function(result) {
+vegaEmbed("#stackedbar_chart", vg_5, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "scatterplot2.vg.json";
+vegaEmbed("#scatterplot2", vg_6, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
